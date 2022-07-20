867192befbb5c747f62e6fae695500eee49f4c12
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-07-21 01:04:43 +0300

    added two js files in the context folder

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../context/theme";
 
-function Interests({ interests, theme }) {
+
+
+function Interests({ interests }) {
+  const { theme } = useContext(ThemeContext);
+
+
+
   return (
     <div>
       <h4>Interests</h4>
@@ -12,5 +19,4 @@ function Interests({ interests, theme }) {
     </div>
   );
 }
-
 export default Interests;
